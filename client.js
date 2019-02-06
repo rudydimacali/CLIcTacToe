793b5daf47f2f3addce82643f02c96591dc412c9
Rudy <rudydimacali@gmail.com>
2019-02-06 15:31:38 -0800

    Complete basic functionality.

--- client.js
+++ client.js
@@ -1,9 +1,4 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const readline = require('readline-sync');
 
 const checkWin = (board) => {
   board.forEach((row, col) => {
@@ -25,27 +20,16 @@ const checkWin = (board) => {
 };
 
 const togglePiece = (board, currentPlayer) => {
-  let x = null;
-  let y = null;
-  rl.question('Enter the zero-indexed X coordinate of the position to mark.', (answer) => {
-    if (answer >= 0 && answer <= 2) {
-      x = answer;
-      rl.close();
-    } else {
-      togglePiece(board, currentPlayer);
-    }
-  });
-  rl.question('Enter the zero-indexed X coordinate of the position to mark.', (answer) => {
-    if (answer >= 0 && answer <= 2) {
-      y = answer;
-      rl.close();
-    } else {
-      togglePiece(board, currentPlayer);
-    }
-  });
-  if (board[x][y] === '_') {
+  let x = readline.question('Enter the zero-indexed X coordinate of the position to mark: ');
+  console.log('\n');
+  let y = readline.question('Enter the zero-indexed Y coordinate of the position to mark: ');
+  console.log('\n');
+  if (x < 0 || x > 2 || y < 0 || y > 2) {
+    togglePiece();
+  } else if (board[x][y] === '_') {
     board[x][y] = currentPlayer;
   } else {
+    console.log('Please enter a valid position that has not been marked!');
     togglePiece(board, currentPlayer);
   }
 };
@@ -54,15 +38,12 @@ const playGame = (board) => {
   let currentPlayer = 'X';
   while (!checkWin(board)) {
     togglePiece(board, currentPlayer);
+    console.log(`${board[0]}\n${board[1]}\n${board[2]}`);
     currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
   }
   console.log(`${currentPlayer} wins!`);
-  let playAgain = 'n';
-  rl.question('Would you like to play again? (Y/N)', (answer) => {
-    playAgain = answer.toLowerCase();
-    rl.close();
-  });
-  if (playAgain === 'y') {
+  let playAgain = readline.question('Would you like to play again? (Y/N)');
+  if (playAgain.toLowerCase() === 'y') {
     playGame([
       ['_', '_', '_'],
       ['_', '_', '_'],
